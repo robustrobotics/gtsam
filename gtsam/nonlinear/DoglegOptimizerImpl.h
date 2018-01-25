dfb12fd89115de7665bb6931b2a05ce05b070b57
Kris Frey <kfrey@mit.edu>
2018-01-25 19:50:42 +0000

    Fix dogleg delta-update logic

--- gtsam/nonlinear/DoglegOptimizerImpl.h
+++ gtsam/nonlinear/DoglegOptimizerImpl.h
@@ -191,8 +191,8 @@ typename DoglegOptimizerImpl::IterationResult DoglegOptimizerImpl::Iterate(
       if(mode == ONE_STEP_PER_ITERATION || mode == SEARCH_REDUCE_ONLY)
         stay = false;   // If not searching, just return with the new Delta
       else if(mode == SEARCH_EACH_ITERATION) {
-        if(fabs(newDelta - Delta) < 1e-15 || lastAction == DECREASED_DELTA)
-          stay = false; // Searching, but Newton's solution is within trust region so keep the same trust region
+        if(Delta - dx_d_norm > 1e-15 || lastAction == DECREASED_DELTA)
+          stay = false; // Searching, but step not limited by Delta so just accept
         else {
           stay = true;  // Searching and increased Delta, so try again to increase Delta
           lastAction = INCREASED_DELTA;
@@ -232,8 +232,9 @@ typename DoglegOptimizerImpl::IterationResult DoglegOptimizerImpl::Iterate(
       // NOTE:  NaN and Inf solutions also will fall into this case, so that we
       // decrease Delta if the solution becomes undetermined.
       assert(0.0 > rho);
-      if(Delta > 1e-5) {
-        Delta *= 0.5;
+      const double dx_d_norm = result.dx_d.norm();
+      if(dx_d_norm > 1e-5) {
+        Delta = 0.5 * dx_d_norm;
         stay = true;
         lastAction = DECREASED_DELTA;
       } else {
